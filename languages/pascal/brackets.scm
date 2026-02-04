ed2348fc0602d928339998a6c18842dcc1300600
lystakata <lystakata@gmail.com>
2026-02-05 00:09:29 +0200

    Reorder and adjust bracket queries
     - Separate case with else highlighting
     - Separate try/finally/except block highlighting
     - Change interface highlighting

--- languages/pascal/brackets.scm
+++ languages/pascal/brackets.scm
@@ -2,26 +2,36 @@
 ("(" @open ")" @close)
 ("[" @open "]" @close)
 
-; Begin/end blocks
-((kBegin) @open (kEnd) @close)
-
-; Case statement blocks  
-((kCase) @open (kEnd) @close)
-
 ; Record blocks
 ((kRecord) @open (kEnd) @close)
 
 ; Class blocks
 ((kClass) @open (kEnd) @close)
 
-; Try/except blocks
+; Interface definitions
+((kInterface) @open (kEnd) @close)
+((kDispInterface) @open (kEnd) @close)
+
+; Begin/end blocks
+((kBegin) @open (kEnd) @close)
+((kBegin) @open (kEndDot) @close)
+
+; Case statement blocks
+((kCase) @open (kElse) @close)
+((kCase) @open (kEnd) @close)
+
+; Try/finally/except blocks (staged)
+((kTry) @open (kFinally) @close)
+((kFinally) @open (kEnd) @close)
+
+((kTry) @open (kExcept) @close)
+((kExcept) @open (kEnd) @close)
+
+; Try/finally/except blocks
 ((kTry) @open (kEnd) @close)
 
 ; Repeat/until blocks
 ((kRepeat) @open (kUntil) @close)
-
-; Interface/implementation sections
-((kInterface) @open (kImplementation) @close)
 
 ; Asm blocks
 ((kAsm) @open (kEnd) @close)
