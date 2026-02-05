419b2d89bee6946bac9d75a6806e29574dde3bab
lystakata <lystakata@gmail.com>
2026-02-05 02:00:49 +0200

    Restructure and overhaul of indentation rules.
     - Test by selecting code, and executing command 'editor: auto indent'

--- languages/pascal/indents.scm
+++ languages/pascal/indents.scm
@@ -1,47 +1,88 @@
 ; Begin/end blocks
 (block (kEnd) @end) @indent
 
+; Declaration sections - var/const/type/uses indent their contents
+(declUses (moduleName)) @indent
+(declConsts (declConst)) @indent
+(declTypes (declType)) @indent
+(declVars (declVar)) @indent
+
+; Inline var declaration
+(varDef (kVar)) @indent
+; Inline var declaration with assignment
+(varAssignDef (kVar)) @indent
+
+; Record type declaration
+(declClass (kRecord) @start (kEnd) @end) @indent
+; Variant record case statement
+(declVariant (_)) @indent
+
+; Helpers declaration
+(declHelper (_) @start (kEnd) @end) @indent
+
+; Classes / Interfaces
+(declIntf (kEnd) @end) @indent
+
+((declSection (_)) @indent)
+;(declSection) @outdent
+;(declClass (declSection)) @outdent
+
+; Object type declaration
+(declClass (kObject) @start (kEnd) @end) @indent
+
 ; Procedure and function bodies
-(defProc body: (block (kEnd) @end)) @indent
-
-; Class and record declarations
-(declClass (kEnd) @end) @indent
-
-; Case statements
-(case (kEnd) @end) @indent
+;(defProc body: (block (kEnd) @end)) @indent
+;begin/end handles it
 
 ; Try/except/finally blocks
-(try (kEnd) @end) @indent
+(try (kFinally) @end) @indent
+(try (kFinally) @start (kEnd) @end) @indent
+
+(try (kExcept) @end) @indent
+(try (kExcept) @start (kEnd) @end) @indent
+(exceptionHandler (kDo)) @indent
 
 ; Repeat/until loops
 (repeat (kUntil) @end) @indent
 
-; Interface and implementation sections
-(interface) @indent
-(implementation) @indent
+; For loops indent all, outdent begin end block
+(for body: (block)) @outdent
+(for body: (_)) @indent
+(foreach body: (block)) @outdent
+(foreach body: (_)) @indent
 
-; If/then/else statements (indent the then/else parts)
-(if then: (_) @indent)
-(ifElse then: (_) @indent)
-(ifElse else: (_) @indent)
+; While/do loops indent all, outdent begin end block
+(while body: (block)) @outdent
+(while body: (_)) @indent
 
-; While/do loops
-(while body: (_) @indent)
+; Case statements indent all, else, outdent begin end block
+(case (kEnd) @end) @indent
+(caseCase body: (block)) @outdent
+(caseCase (_)) @indent
+(case (kElse) @start (kEnd) @end) @indent
 
-; For loops
-(for body: (_) @indent)
-(foreach body: (_) @indent)
+; If/then/else statements
+(if then: (block)) @outdent
+(if then: (_)) @indent
 
-; With statements
-(with body: (_) @indent)
+; ifElse statement
+(ifElse then: (block)) @outdent
+(ifElse then: (_) ) @indent
+(ifElse else: (statement) @indent)
+(ifElse else: (assignment) @end) @indent
+
+; Anonymous method assigned to variable, indent following block
+(assignment (lambda)) @indent
+
+; With statements indent all, outdent begin end block
+(with body: (block)) @outdent
+(with body: (_)) @indent
 
 ; Asm blocks
 (asm (kEnd) @end) @indent
 
-; Declaration sections - indent their contents
-(declSection (_) @indent)
+; initialization/finalization blocks
+(initialization (_)) @indent
+(finalization (_)) @indent
 
-; Var/const/type sections - indent their contents
-(declVars (_) @indent)
-(declConsts (_) @indent)
-(declTypes (_) @indent)
+(kEndDot) @outdent
